7be06c9325754198164b90b6e05bf50a9e3f9d36
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-11-08 12:08:06 -0500

    chore: try extension of from_context

--- examples/extend_ctx.rs
+++ examples/extend_ctx.rs
@@ -2,42 +2,65 @@
 #![allow(unused)]
 
 use cream::{
-    context::{ContextProvide, CreamContext},
+    context::{ContextProvide, ContextProvide2, CreamContext, FromContext2},
     event_bus::EventBusPort,
 };
 
-struct OtherCtx {
-    port: EventBusPort,
-}
+trait Ctx: ContextProvide2<Dep> + ContextProvide2<Dep2> {}
 
-impl ContextProvide<EventBusPort> for OtherCtx {
-    fn ctx_provide(&self) -> EventBusPort {
-        self.port.clone()
-    }
-}
-
-struct Ctx {
-    cream_ctx: CreamContext,
-    other: OtherCtx,
+struct MyCtx {
     dep: Dep,
 }
 
-impl<S> ContextProvide<S> for Ctx
-where
-    CreamContext: ContextProvide<S>,
-{
-    fn ctx_provide(&self) -> S {
-        self.cream_ctx.ctx_provide()
+impl Ctx for MyCtx {}
+impl FromContext2<MyCtx> for Dep {
+    fn from_ctx(ctx: &MyCtx) -> Self {
+        ctx.dep.clone()
+    }
+}
+
+impl FromContext2<MyCtx> for Dep2 {
+    fn from_ctx(ctx: &MyCtx) -> Self {
+        Dep2
     }
 }
 
 #[derive(Clone)]
 struct Dep;
 
-impl ContextProvide<Dep> for Ctx {
-    fn ctx_provide(&self) -> Dep {
-        self.dep.clone()
+#[derive(Clone)]
+struct Dep2;
+
+#[derive(Clone)]
+struct Service1 {
+    dep: Dep,
+    dep2: Dep2,
+}
+
+impl<C: Ctx> FromContext2<C> for Service1 {
+    fn from_ctx(ctx: &C) -> Self {
+        Self {
+            dep: ctx.ctx_provide(),
+            dep2: ctx.ctx_provide(),
+        }
     }
 }
 
-fn main() {}
+#[derive(Clone)]
+struct Service2 {
+    dep: Dep,
+    service1: Service1,
+}
+
+impl<C: Ctx> FromContext2<C> for Service2 {
+    fn from_ctx(ctx: &C) -> Self {
+        Self {
+            dep: ctx.ctx_provide(),
+            service1: ctx.ctx_provide(),
+        }
+    }
+}
+
+fn main() {
+    let a = Service2::from_ctx(&MyCtx { dep: Dep });
+}
